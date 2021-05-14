5d6c673e7c6346331d3330ef5d24c14dab7ef6d7
Subhajit Sahu <wolfram77@gmail.com>
2021-05-14 10:34:23 +0530

    :skunk: w/ temporal-networks

--- bin/_fetch.sh
+++ bin/_fetch.sh
@@ -2,6 +2,7 @@
 
 rename() {
   if   [[ "$2" == "$1" ]]; then
+    :
   elif [[ "$2" == *"/" ]]; then
     cp -rf "$1"/* "$2"
     rm -rf "$1"
@@ -26,65 +27,10 @@ fetch() {
   num="$(ls -1 "$dst".* 2>/dev/null | wc -l)"
   if [ ! -e "$dst" ] || [[ "$num" == "0" ]]; then
     echo "Fetching $url ..."
-    wget -q "$url"
+    wget "$url"
     extract "$dow"
     rename "$nam" "$dst"
   fi
 }
 
-fetchPlain() {
-  url="$1"
-  nam="${url##*/}"
-  dst="${2:-nam}"
-  if [ ! -e "$dst" ]; then
-    echo "Fetching $url ..."
-    wget -q "$url"
-    rename "$nam" "$dst"
-  fi
-}
-
-
-fetchGz() {
-  url="$1"
-  dow="${url##*/}"
-  nam="${dow%%.gz}"
-  dst="${2:-nam}"
-  if [ ! -e "$dst" ]; then
-    echo "Fetching $url ..."
-    wget -q "$url"
-    gunzip "$dow"
-    rename "$nam" "$dst"
-  fi
-}
-
-
-fetchTarGz() {
-  url="$1"
-  dow="${url##*/}"
-  nam="${dow%%.tar.gz}"
-  dst="${2:-nam}"
-  num="$(ls -1 "$dst".* 2>/dev/null | wc -l)"
-  if [ ! -e "$dst" ] || [[ "$num" == "0" ]]; then
-    echo "Fetching $url ..."
-    wget -q "$url"
-    tar -xzf "$dow"
-    rm -f "$dow"
-    rename "$nam" "$dst"
-  fi
-}
-
-
-fetchZip() {
-  url="$1"
-  dow="${url##*/}"
-  nam="${dow%%.zip}"
-  dst="${2:-nam}"
-  num="$(ls -1 "$dst".* 2>/dev/null | wc -l)"
-  if [ ! -e "$dst" ] || [[ "$num" == "0" ]]; then
-    echo "Fetching $url ..."
-    wget -q "$url"
-    tar -xzf "$dow"
-    rm -f "$dow"
-    rename "$nam" "$dst"
-  fi
-}
+if [[ "$1" != "" ]]; then fetch "$@"; fi
--- bin/temporal-networks.sh
+++ bin/temporal-networks.sh
@@ -1,9 +1,65 @@
 #!/usr/bin/env bash
-fetch() {
-  if [[ "" ]]
-}
+dir="$(dirname "$(readlink -f "$0")")"
+source "$dir/_fetch.sh"
+
+# Switch directory
+if [[ "$1" != "" ]]; then pushd "$1" > /dev/null; fi
 
 
 # Social Network: Reddit Hyperlink Network
-wget http://snap.stanford.edu/data/soc-redditHyperlinks-body.tsv
-wget http://snap.stanford.edu/data/soc-redditHyperlinks-title.tsv
+fetch http://snap.stanford.edu/data/soc-redditHyperlinks-body.tsv
+fetch http://snap.stanford.edu/data/soc-redditHyperlinks-title.tsv
+
+# Stack Overflow temporal network
+fetch http://snap.stanford.edu/data/sx-stackoverflow.txt.gz
+fetch http://snap.stanford.edu/data/sx-stackoverflow-a2q.txt.gz
+fetch http://snap.stanford.edu/data/sx-stackoverflow-c2q.txt.gz
+fetch http://snap.stanford.edu/data/sx-stackoverflow-c2a.txt.gz
+
+# Math Overflow temporal network
+fetch http://snap.stanford.edu/data/sx-mathoverflow.txt.gz
+fetch http://snap.stanford.edu/data/sx-mathoverflow-a2q.txt.gz
+fetch http://snap.stanford.edu/data/sx-mathoverflow-c2q.txt.gz
+fetch http://snap.stanford.edu/data/sx-mathoverflow-c2a.txt.gz
+
+# Super User temporal network
+fetch http://snap.stanford.edu/data/sx-superuser.txt.gz
+fetch http://snap.stanford.edu/data/sx-superuser-a2q.txt.gz
+fetch http://snap.stanford.edu/data/sx-superuser-c2q.txt.gz
+fetch http://snap.stanford.edu/data/sx-superuser-c2a.txt.gz
+
+# Ask Ubuntu temporal network
+fetch http://snap.stanford.edu/data/sx-askubuntu.txt.gz
+fetch http://snap.stanford.edu/data/sx-askubuntu-a2q.txt.gz
+fetch http://snap.stanford.edu/data/sx-askubuntu-c2q.txt.gz
+fetch http://snap.stanford.edu/data/sx-askubuntu-c2a.txt.gz
+
+# wiki-talk temporal network
+fetch http://snap.stanford.edu/data/wiki-talk-temporal.txt.gz
+fetch http://snap.stanford.edu/data/wiki-talk-temporal-usernames.txt.gz
+
+# email-Eu-core temporal network
+fetch http://snap.stanford.edu/data/email-Eu-core-temporal.txt.gz
+fetch http://snap.stanford.edu/data/email-Eu-core-temporal-Dept1.txt.gz
+fetch http://snap.stanford.edu/data/email-Eu-core-temporal-Dept2.txt.gz
+fetch http://snap.stanford.edu/data/email-Eu-core-temporal-Dept3.txt.gz
+fetch http://snap.stanford.edu/data/email-Eu-core-temporal-Dept4.txt.gz
+
+# CollegeMsg temporal network
+fetch http://snap.stanford.edu/data/CollegeMsg.txt.gz
+
+# Bitcoin OTC trust weighted signed network
+fetch https://snap.stanford.edu/data/soc-sign-bitcoinotc.csv.gz
+
+# Bitcoin Alpha trust weighted signed network
+fetch https://snap.stanford.edu/data/soc-sign-bitcoinalpha.csv.gz
+
+# Social Network: MOOC User Action Dataset
+fetch http://snap.stanford.edu/data/act-mooc.tar.gz act-mooc/
+
+# Dynamic Face-to-Face Interaction Networks
+fetch http://snap.stanford.edu/data/comm-f2f-Resistance-network.tar.gz
+
+
+# Switch directory
+if [[ "$1" != "" ]]; then popd > /dev/null; fi
